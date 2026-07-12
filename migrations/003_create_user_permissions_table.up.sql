54151580a7856a7a84a834f895b24b9ff279e15c
balla-achila <balla@jobalsoftware.com>
2026-07-13 00:31:59 +0300

    Update

--- migrations/003_create_user_permissions_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE login_attempts (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(100),
-    ip_address VARCHAR(45),
-    success BOOLEAN DEFAULT FALSE,
-    attempt_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE INDEX idx_login_attempts_user ON login_attempts(username, attempt_time);
